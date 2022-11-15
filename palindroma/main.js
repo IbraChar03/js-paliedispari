8e09477875cc2688cbe988da7e0c5751a1a3c841
Ibrahim <ibrachar68@gmail.com>
2022-11-15 16:29:45 +0100

    input e button

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,20 +1,27 @@
-let parolaUtente=prompt("inserisci parola");
-if(typeof parolaUtente === 'number'){
-  alert("Attenzione! Inserisci una parola non dei numeri");
-  location.reload();
-}
-let parolaInvertita=invertiParola(parolaUtente);
 
- if(parolaUtente == parolaInvertita){
-    console.log('la parola è palindroma');
-   } else {
-     console.log('la parola non è palindroma');
-   }
-  
- function invertiParola(parola){
-   let parolaInv = parola.split('').reverse().join('');  
-   return parolaInv;
+ let button=document.getElementById("button");
+ button.addEventListener("click",
+ function(){
+  let parolaUtente=document.getElementById("input").value;
+  let title=document.getElementById("title");
+  let parolaInvertita=invertiParola(parolaUtente);
+ 
+   if(parolaUtente == parolaInvertita){
+      console.log('la parola è palindroma');
+      title.innerHTML="la parola è palindroma";
+
+     } else {
+       console.log('la parola non è palindroma');
+       title.innerHTML="la parola non è palindroma";
+     }
+   
  }
+ 
+ );
 
 
 
+ function invertiParola(parola){
+    let parolaInv = parola.split('').reverse().join('');  
+    return parolaInv;
+ }
