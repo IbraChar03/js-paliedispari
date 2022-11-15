b9576bef4468e31d9347479f1ef01c83d14841e7
Ibrahim <ibrachar68@gmail.com>
2022-11-15 13:56:46 +0100

    fine esercizio pari dispari in console

---
+++ pari dispari/main.js
@@ -0,0 +1,35 @@
+
+let paridispari=prompt("scegli pari o dispari");
+let numUtente=parseInt(prompt("inserisci un numero da 1 a 5"));
+console.log(numUtente);
+let numrandompc=numeropc();
+console.log(numrandompc);
+let somma=numUtente + numrandompc;
+console.log(somma);
+let risultato=sommaPariDispari();
+console.log(risultato);
+
+function numeropc(){
+  let numrandom2=Math.floor(Math.random()*5)+1;
+  return numrandom2;
+
+}
+function sommaPariDispari(){
+  if(somma %2 ==0 && paridispari=="pari"){
+    return "hai vinto la somma è pari"
+  }
+  else  if(somma %2 ==0 && paridispari=="dispari"){
+    return "hai perso la somma è pari"
+  }
+  else  if(somma %2 ==1 && paridispari=="pari"){
+    return "hai perso la somma è dispari"
+  }
+
+  else  if(somma %2 ==1 && paridispari=="dispari"){
+    return "hai vinto la somma è dispari"
+  }
+
+
+ 
+
+}
